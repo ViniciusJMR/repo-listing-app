48d04802980d59902504642e12eaca57d91d8e16
Vinicius Rodrigues <viniciusrodrigues@pop-os.localdomain>
2022-08-04 20:12:38 -0300

    Created and implemented search action bar

--- app/src/main/java/me/dio/vinicius/repolistingapp/ui/MainActivity.kt
+++ app/src/main/java/me/dio/vinicius/repolistingapp/ui/MainActivity.kt
@@ -2,11 +2,40 @@ package me.dio.vinicius.repolistingapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.widget.SearchView
 import me.dio.vinicius.repolistingapp.R
+import me.dio.vinicius.repolistingapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        val searchView = (menu.findItem(R.id.action_search).actionView as SearchView)
+        searchView.setOnQueryTextListener(this)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        Log.d(TAG, "onQueryTextSubmit: $query")
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        Log.d(TAG, "onQueryTextChange: $newText")
+        return false
+    }
+
+    companion object {
+        private const val TAG = "TAG"
     }
 }
