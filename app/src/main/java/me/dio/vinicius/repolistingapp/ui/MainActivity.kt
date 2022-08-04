1d86e8cfc3f00f0d0216252072e1bcd02409d6e5
Vinicius Rodrigues <viniciusrodrigues@pop-os.localdomain>
2022-08-04 19:09:51 -0300

    Implemented starting config

--- app/src/main/java/me/dio/vinicius/repolistingapp/ui/MainActivity.kt
+++ app/src/main/java/me/dio/vinicius/repolistingapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package me.dio.vinicius.repolistingapp
+package me.dio.vinicius.repolistingapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import me.dio.vinicius.repolistingapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
